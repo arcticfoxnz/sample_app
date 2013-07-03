fe9fd68db4fd03acec2e7978862f024bacb214de
arcticfoxnz <arcticfoxnz@gmail.com>
2013-07-03 13:27:09 +1200

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,6 +41,8 @@ before do
     it { should_not be_valid }
   end
   
+# EMAIL TESTS
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -80,6 +82,18 @@ before do
 
     it { should_not be_valid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
+# PASSWORD TESTS
 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
