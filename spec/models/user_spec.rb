fbb9128a357dd83224847516fc1930dca73f0a6d
arcticfoxnz <arcticfoxnz@gmail.com>
2013-07-03 18:20:54 +1200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@
 require 'spec_helper'
 
 describe User do
-before do
+  before do
     @user = User.new(name: "Example User", email: "user@example.com", 
                      password: "foobar", password_confirmation: "foobar")
   end
@@ -25,6 +25,15 @@ before do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
